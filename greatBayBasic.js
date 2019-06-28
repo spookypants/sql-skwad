c5ec74d9cfdaeaede038f08d42c27e4eddcb65bf
Gabe <42557432+spookypants@users.noreply.github.com>
2019-06-28 14:03:38 -0500

    initial database linking functions

--- greatBayBasic.js
+++ greatBayBasic.js
@@ -15,3 +15,53 @@ var connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: "greatBaySchema"
 });
+
+// POST ITEM UPDATE
+function postItem() {
+    // console.log("Inserting a new product...\n");
+    var query = connection.query(
+      "INSERT INTO items SET ?",
+      {
+        name: // user input name (from inquirer),
+        description: // user input desc (from inquirer),
+        lowestBid: //user input starting bid (from inquirer)
+      },
+      function(err, res) {
+        if (err) throw err;
+        // console.log(res.affectedRows + " product inserted!\n");
+      }
+    );
+    // logs the actual query being run
+    // console.log(query.sql);
+  }
+
+// LOOK AT ITEMS - WHEN USER SELECTS BID ON ITEMS
+  // SHOW LIST OF POSSIBLE BID ITEMS
+function showItems() {
+    var query = connection.query(
+        "SELECT name FROM items", function(err, res) {
+            if(err) throw err;
+            console.log(res);
+        }
+    )
+}
+
+// BID UPDATE
+function updateBid() {
+    // console.log("Updating all Rocky Road quantities...\n");
+    var query = connection.query(
+      "UPDATE items SET ? WHERE ?",
+      [
+        {
+          highestBid: //user bid
+        },
+        {
+          name: //user selected item
+        }
+      ],
+      function(err, res) {
+        if (err) throw err;
+        // console.log(res.affectedRows + " bid successful!\n");
+      }
+    );
+  }
