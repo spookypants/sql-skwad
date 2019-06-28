92c23ca4f1f92cc1c8e7f9e05c95ff24f8def61a
elaintran <elain.ttran@gmail.com>
2019-06-28 13:08:39 -0500

    updated process.env

--- greatBayBasic.js
+++ greatBayBasic.js
@@ -12,6 +12,6 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "port.env.DB_PASSWORD",
+  password: process.env.DB_PASSWORD,
   database: "greatBaySchema"
 });
